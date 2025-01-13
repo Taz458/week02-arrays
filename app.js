582304e7c1992cec584ba31ed69a10b421b100a0
Taran Sahota <tazsahota2003@gmail.com>
2025-01-13 14:04:20 +0000

    workshop for arrays

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+
+const blogPosts =[
+    "My first blogpost",
+    "my second blogpost",
+    "my third blog post",
+
+];
+
+
+console.log(blogPosts[0],blogPosts[1],blogPosts[2]) //accessing items
+
+blogPosts[3] = "My fourth blog post" //adding items
+
+console.log(blogPosts)
+
